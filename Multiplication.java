3f7780b85e68e03d5ebd33926194f2223337d56a
Keerthisreenidhi <sreenidhikeerthi1206@gmail.com>
2025-01-04 09:55:46 +0530

    Update Multiplication.java3

--- Multiplication.java
+++ Multiplication.java
@@ -10,4 +10,28 @@ public class Multiplication {
         System.out.println("The result of multiplication is: " + result);
         scanner.close();
     }
+
+}
+
+
+
+
+class Test {
+    // Method to find factorial
+    // of given number
+    static int factorial(int n)
+    {
+        int res = 1, i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
+    }
+ 
+    // main method
+    public static void main(String[] args)
+    {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is "
+                           + factorial(5));
+    }
 }
